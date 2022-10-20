4f3678f5e398b1f3420305d776a0af55b082f75e
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-20 09:23:58 -0600

    remove image section from card

--- src/App.js
+++ src/App.js
@@ -26,10 +26,6 @@ function App() {
           return (
             <Card className="pokemon-card" key={id}>
               <Card.Header>Pokemon ID: {getIdFromUrl(url)}</Card.Header>
-              <Card.Img
-                variant="top"
-                src="https://via.placeholder.com/350x150"
-              />
               <Card.Body>
                 <Card.Title>{formatName(name)}</Card.Title>
                 <Card.Text>Description goes here.</Card.Text>
