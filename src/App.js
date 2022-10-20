6209f8cd12f57f75536df2097c1f6ded2aa4e6ee
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-20 09:56:46 -0600

    Update app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <main className="pokemon-cards-container"></main>
+      <main className="pokemon-cards-container">hello world</main>
     </div>
   );
 }
