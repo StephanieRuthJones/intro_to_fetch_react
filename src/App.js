9c6f8ded1b393a0eb6a154318b4abebc03e59349
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-20 09:37:38 -0600

    Remove comments

--- src/App.js
+++ src/App.js
@@ -47,33 +47,3 @@ function App() {
 }
 
 export default App;
-
-// import { useState, useEffect } from "react";
-// import "./App.css";
-// const baseUrl = "https://pokeapi.co/api/v2/pokemon";
-// function App() {
-//   const [pokemonList, setPokemonList] = useState([]);
-//   const createPokemonObject = (results) => {
-//     results.forEach(async (pokemon) => {
-//       const response = await fetch(`${baseUrl}/${pokemon.name}`);
-//       const pokemonStats = await response.json();
-//       // console.log("pokemon name", pokemonStats);
-//       const { sprites, name } = pokemonStats;
-//       const { front_default } = sprites;
-//       setPokemonList((prev) => [...prev, { name, img: front_default }]);
-//     });
-//   };
-//   const getAllPokemons = async () => {
-//     const response = await fetch(`${baseUrl}?limit=20`);
-//     const data = await response.json();
-//     // console.log("allPokemons", data);
-//     createPokemonObject(data.results);
-//   };
-//   useEffect(() => {
-//     getAllPokemons();
-//   }, []);
-//   console.log("pokemonList", pokemonList);
-//   return <div className="App"></div>;
-// }
-
-// export default App;
